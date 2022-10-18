d1c09f6251583b7bd0f5930b24498b7c1929a096
Carlos Toledo <carlombo@hotmail.com>
2022-10-18 00:12:54 -0300

    Models User and Friend created

---
+++ db/migrate/20221018031101_create_friends.rb
@@ -0,0 +1,10 @@
+class CreateFriends < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friends do |t|
+      t.string :friend_name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
